0256f7b92b5c3325146c6c1c0f26e81e3f5da9cf
vangheem <vangheem@gmail.com>
2017-12-15 12:27:37 -0500

    Back to development: 1.0.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='guillotina_rediscache',
-    version='1.0.13',
+    version='1.0.14.dev0',
     description='guillotina cache implementation using redis + lru in-memory cache',
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGELOG.rst').read()),
