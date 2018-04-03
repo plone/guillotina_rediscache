754ae7bbdbf4c59bbe69a6b942d1031146286504
vangheem <vangheem@gmail.com>
2018-04-03 12:53:34 -0400

    Preparing release 1.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='guillotina_rediscache',
-    version='1.3.4.dev0',
+    version='1.3.4',
     description='guillotina cache implementation using '
                 'redis + lru in-memory cache',
     long_description=(open('README.rst').read() + '\n' +
