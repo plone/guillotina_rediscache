f7f755c86e80663690cbedbef72453f794c2d21a
vangheem <vangheem@gmail.com>
2018-11-20 10:21:24 -0500

    Back to development: 2.1.1
    Co-authored-by: Wildcard Corp. <corporate@wildcardcorp.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ module1 = Extension(
 
 setup(
     name='guillotina_rediscache',
-    version='2.1.0',
+    version='2.1.1.dev0',
     description='guillotina cache implementation using '
                 'redis + lru in-memory cache',
     long_description=(open('README.rst').read() + '\n' +
